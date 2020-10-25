08ff901e1ce39cb5adb416474accf740c574b17a
JNickBaughan <JNickBaughan@gmail.com>
2020-10-25 17:59:15 -0400

    add old resume site to git

---
+++ Javascripts/sectionConfig.js
@@ -0,0 +1,19 @@
+config = [
+
+			{
+				sectionID: "bioSection"
+				,data: {
+					title: "Bio"
+					,sectionTitleOne : "Introduction"
+					,sectionOneContent : "Nick here. UI developer working with CoStar Group in Richmond, Va"
+					,sectionTitleTwo: "Skills & Interests"
+					,sectionTwoContent: "Well versed in JavaScript. Working with the React/Redux stack daily."
+				}
+				,template: "<span class='contentTitle'>{{title}}</span><div id='content_bio' class='bs-callout bs-callout-default startContainer sectionEnd container shadowContainer'><div class='row'><div class='col-md-6 col-xs-8'><br/><br/><h5>{{sectionTitleOne}}</h5>{{sectionOneContent}} <br/><br/><H5>{{sectionTitleTwo}}</H5>{{sectionTwoContent}}<br/><br/></div></div></div>"
+			}
+			
+			
+
+
+
+]
